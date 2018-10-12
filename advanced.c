50aea39177b136d4e7e4115a26590d352a9dcce0
LeeYellow <emma30533@gmail.com>
2018-10-12 22:14:43 +0800

    while version

--- advanced.c
+++ advanced.c
@@ -11,12 +11,13 @@ float* recv;
 int List_size_in_process, front, rest;
 int RecvPrev, RecvNext; 
 
-void merge_L(){ 
+void merge_L(char* isSorted){ 
   int recv_ptr = RecvPrev - 1;
   int arr_ptr = List_size_in_process - 1;
   for(int tmp_ptr = List_size_in_process-1; tmp_ptr >=0; tmp_ptr--){ 
     if(recv_ptr>=0 && arr[arr_ptr] < recv[recv_ptr]){
       tmp[tmp_ptr] = recv[recv_ptr];
+	    *isSorted = 0;
       --recv_ptr;
     }else{
       tmp[tmp_ptr] = arr[arr_ptr];
@@ -30,12 +31,13 @@ void merge_L(){
 
 }
 
-void merge_R(){ 
+void merge_R(char* isSorted){ 
   int recv_ptr = 0;
   int arr_ptr = 0;
   for(int tmp_ptr = 0; tmp_ptr < List_size_in_process; tmp_ptr++){
     if(recv_ptr< RecvNext && recv[recv_ptr] < arr[arr_ptr]){
           tmp[tmp_ptr] = recv[recv_ptr];
+      		*isSorted = 0;
           ++recv_ptr;
     }else{
       tmp[tmp_ptr] = arr[arr_ptr];
@@ -82,6 +84,7 @@ int main(int argc, char *argv[])
 	}
   
 	rest = N%size;
+
 		
   List_size_in_process = N/size;
   if(rank<rest) {++List_size_in_process;
@@ -114,32 +117,31 @@ int main(int argc, char *argv[])
 	//sort inside 
 	qsort(arr, List_size_in_process, sizeof(float), cmp);
 	
-	//char tmp2, isSorted = 0;
-	//while(isSorted==0){
-  for(int j = 0; j < size; j++){
-		//isSorted = 1;
+	char tmp2, isSorted = 0;
+	while(isSorted==0){
+		isSorted = 1;
 		if(rank%2){
 			if(rank > 0){ 
 				MPI_Sendrecv(arr,List_size_in_process,MPI_FLOAT,rank-1,1,recv,RecvPrev,MPI_FLOAT,rank-1,0,custom_world,MPI_STATUS_IGNORE);
-				merge_L();
+				merge_L(&isSorted);
 			}
 
 			if(rank != size-1){	
 				MPI_Sendrecv(arr,List_size_in_process,MPI_FLOAT,rank+1,0,recv,RecvNext,MPI_FLOAT,rank+1,1,custom_world,MPI_STATUS_IGNORE);
-				merge_R();
+				merge_R(&isSorted);
 			}
 		}else{
 			if(rank != size-1){	
 				MPI_Sendrecv(arr,List_size_in_process,MPI_FLOAT,rank+1,0,recv,RecvNext,MPI_FLOAT,rank+1,1,custom_world,MPI_STATUS_IGNORE);			
-  			merge_R();
+  			merge_R(&isSorted);
 			}
 			if(rank > 0){ 
 				MPI_Sendrecv(arr,List_size_in_process,MPI_FLOAT,rank-1,1,recv,RecvPrev,MPI_FLOAT,rank-1,0,custom_world,MPI_STATUS_IGNORE);
-				merge_L();
+				merge_L(&isSorted);
 			}
 		}
-    //tmp2=isSorted;
-    //MPI_Allreduce(&tmp2, &isSorted, 1,MPI_CHAR, MPI_BAND, custom_world);
+    tmp2=isSorted;
+    MPI_Allreduce(&tmp2, &isSorted, 1,MPI_CHAR, MPI_BAND, custom_world);
 	}
   
 	MPI_File_open(custom_world, argv[3], MPI_MODE_CREATE|MPI_MODE_WRONLY , MPI_INFO_NULL, &file_out);
